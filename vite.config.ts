b770396f2c547476ce6d08361668cfd85ea0c477
sammwy <sammwy.dev@gmail.com>
2026-04-04 12:34:36 -0300

    fix: added sdk depth

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+function getSDKDir() {
+    const path = require("path");
+    const fs = require("fs");
+    const maxDepth = 5;
+
+    for (let i = 0; i < maxDepth; i++) {
+        const sdkPath = path.join(__dirname, "../".repeat(i) + "sdk");
+        if (fs.existsSync(sdkPath) && fs.existsSync(path.join(sdkPath, "package.json"))) {
+            return sdkPath;
+        }
+    }
+
+    return null;
+}
+
+function resolveSDK(path: string) {
+    const sdkDir = getSDKDir();
+    if (!sdkDir) {
+        throw new Error("SDK not found");
+    }
+    return resolve(sdkDir, path);
+}
+
+
 export default defineConfig({
     base: "./",
     build: {
@@ -9,8 +33,8 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "@staroverlay/sdk/tmi": resolve(__dirname, "../sdk/dist/tmi.mjs"),
-            "@staroverlay/sdk": resolve(__dirname, "../sdk/dist/staroverlay.mjs")
+            "@staroverlay/sdk/tmi": resolveSDK("dist/tmi.mjs"),
+            "@staroverlay/sdk": resolveSDK("dist/staroverlay.mjs")
         }
     }
 });
